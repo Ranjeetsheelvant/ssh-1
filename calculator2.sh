59293996c5764b29b682ba69193ad8bb85fd18b8
Ubuntu <ubuntu@ip-172-31-1-240.ap-south-1.compute.internal>
2022-06-21 04:19:13 +0000

    calculator2

---
+++ calculator2.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+echo "enter two number"
+read a 
+read b
+
+echo "enter choice:"
+echo "1.addition"
+echo "2.division"
+read ch
+
+case $ch in
+	1)res=`echo $a + $b | bc`
+		;;
+
+	2)res=`echo"scale=2; $a / $b" | bc`
+		;;
+esac
+echo "result: $res"
+
+
