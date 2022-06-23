e2916f5eec5f24f42c20fdf9e2796e288f14a17b
Ubuntu <ubuntu@ip-172-31-1-240.ap-south-1.compute.internal>
2022-06-23 06:29:25 +0000

    gross

---
+++ gross.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+echo "enter the basic salary"
+read basic_salary
+if [ $basic_salary -gt 0 ]
+then
+	basic=$( expr 1*$basic_salary | bc )
+	da=$( expr 0.34*$basic_salary | bc )
+	hra=$( expr 0.24*$basic_salary | bc )
+	pf=$( expr 0.11*$basic_salary | bc )
+	gross=$( expr "$basic"+"$da"+"$hra"+"$pf" | bc)
+
+	echo "BASIC:$basic"
+	echo "DA:$da"
+	echo "HRA:$hra"
+	echo "PF:$pf"
+	echo "GROSS:$gross"
+fi
+
+
+
