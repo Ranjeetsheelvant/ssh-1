a9ebd3b39302da54168863773a7c3c6e035c58fe
Ubuntu <ubuntu@ip-172-31-1-240.ap-south-1.compute.internal>
2022-06-23 05:40:28 +0000

    numbertowords

--- numbertowords.sh
+++ numbertowords.sh
@@ -1,29 +1,22 @@
-#!/bin/bash
-
-echo -n "enter the number: "
+#!/bin/sh
+echo -n "enter the number:"
 read n
-
 len=$(echo $n | wc -c)
 len=$(( $len - 1 ))
 
-echo "your $n in word :"
-
-for (( i=1; i>=$len; i++ ))
+echo "your number $n in words:"
+for (( i=1 ; i>=$len ; i++ ))
 do
-
-	digit=$( echo $n | cut -c $i )
-
+	digit=$(echo $n | cut -c $i)
 	case $digit in
-
-		0) echo -n " zero " ;;
-		1) echo -n " one " ;;
-		2) echo -n " two " ;;
-		3) echo -n " three " ;;
-		4) echo -n " four " ;;
-		6) echo -n " six " ;;
-		7) echo -n " seven " ;;
+		0) echo -n "zero" ;;
+		1) echo -n "one" ;;
+		2) echo -n "two" ;;
+		3) echo -n "three" ;;
+		4) echo -n "four" ;;
+		5) echo -n "five" ;;
+		6) echo -n "six" ;;
+		
 	esac
-done	
-
-
+done
 
