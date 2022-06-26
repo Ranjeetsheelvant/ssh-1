9ff05662b4dd5ba7f8203bb685053331e1d11c1b
Your Name <you@example.com>
2022-06-26 13:11:29 +0000

    explain

---
+++ explain.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+$0 - script name 
+$n - nth argument
+$# - display number of argument used 
+$* - display all the arguments in single string
+$@ - display all the arguments in different strings
+$? - this is the exit status of the last execyted command
+$$ - display PPID of current shell
+$! - display PPID of last background command
+ 
+
